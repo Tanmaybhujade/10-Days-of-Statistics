e4a43185301c7d20dd1763320100da70fc23c0c7
Tanmay_bhujade <80476913+Tanmaybhujade@users.noreply.github.com>
2021-06-27 16:42:29 +0530

    Update Day 5: Poisson Distribution I.py

--- Day - 5/Day 5: Poisson Distribution I.py
+++ Day - 5/Day 5: Poisson Distribution I.py
@@ -1,21 +1,18 @@
-# assembled at this plant in:
-# 1. Less than 19.5 hours?
-# 2. Between 20 and 22 hours?
-
-# Import library
-import math
+# Question: A random variable, X, follows Poisson distribution with mean of 2.5
+# Find the probability with which the random variable X is equal to 5.
 
 # Define functions
-def cumulative(mean, std, value):
-    return 0.5 * (1 + math.erf((value - mean) / (std * (2 ** 0.5))))
+def factorial(n):
+    if n == 1 or n == 0:
+        return 1
+    if n > 1:
+        return factorial(n - 1) * n
 
 # Set data
-initial_values = list(map(float, input().split()))
-mean = initial_values[0]
-std = initial_values[1]
-less_period = float(input())
-between_period = list(map(float, input().split()))
+mean = float(input())
+k = float(input())
+e = 2.71828
 
 # Gets the result and show on the screen
-print (round(cumulative(mean, std, less_period),3))
-print (round(cumulative(mean, std, between_period[1]) - cumulative(mean, std, between_period[0]), 3))
+result = ((mean ** k) * (e ** -mean)) /  factorial(k)
+print(round(result, 3))
