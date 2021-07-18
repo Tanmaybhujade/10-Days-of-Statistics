82c5c8adddf3897cd492080d432a406aca1651c6
Tanmay_bhujade <80476913+Tanmaybhujade@users.noreply.github.com>
2021-07-18 22:12:05 +0530

    Update Day-1_Quartile.py

--- Day - 1/Day-1_Quartile.py
+++ Day - 1/Day-1_Quartile.py
@@ -18,4 +18,7 @@ else:
     data_low = numbers[0:int(size/2)]
     data_high = numbers[int(size/2)+1:size]
 
-
+# Get the final result and print on the screen
+print (median(len(data_low), data_low))
+print (median(size, numbers))
+print (median(len(data_high), data_high))
